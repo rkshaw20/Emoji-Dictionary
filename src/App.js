c9fa9c469cae2ae781c2f22d52422e1fc32755ef
Raj Kishor Shaw <73245914+rkshaw20@users.noreply.github.com>
2022-07-14 12:26:05 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,63 @@
+import React, { useState } from "react";
+import "./styles.css";
+
+const emojiDictionary = {
+  "😊": "Smiling",
+  "😳": "disbelief",
+  "😔": "sad",
+  "🥡": "takeout box",
+  "❤️": "love",
+  "😑": "annoyance"
+};
+
+/*
+ * converting an object to array of keys
+ */
+const emojis = Object.keys(emojiDictionary);
+
+export default function App() {
+  const [emoji, setEmoji] = useState("");
+  const [meaning, setmeaning] = useState("Translation will appear here");
+
+  function changeHandler(event) {
+    const inputEmoji = event.target.value;
+    setEmoji(inputEmoji);
+    if (inputEmoji in emojiDictionary) {
+      setmeaning(emojiDictionary[inputEmoji]);
+    } else {
+      setmeaning(
+        "failed to recognise this emoji, please try from the list given below"
+      );
+    }
+  }
+
+  function emojiClickHandler(inputEmoji) {
+    setmeaning(emojiDictionary[inputEmoji]);
+  }
+
+  return (
+    <div className="App">
+      <h1 class="heading">Emoji Dictionary</h1>
+      <input
+        onChange={changeHandler}
+        value={emoji}
+        placeholder={"Enter your emoji"}
+        // style={{
+        //   padding: "1rem",
+        //   minWidth: "80%"
+        // }}
+      />
+      <h2> {emoji}</h2>
+      <h3> {meaning}</h3>
+      {emojis.map((emoji) => (
+        <span
+          onClick={() => emojiClickHandler(emoji)}
+          style={{ fontSize: "2rem", padding: "0.5rem", cursor: "pointer" }}
+        >
+          {" "}
+          {emoji}{" "}
+        </span>
+      ))}
+    </div>
+  );
+}
